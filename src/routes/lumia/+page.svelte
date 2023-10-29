70a8e694ba9ababaa70fea2097b650ff8460ffa1
Johnson Andrew Siziba <sizibajohnsona@gmail.com>
2023-10-29 19:08:26 +0200

    Lumia privacy policy

---
+++ src/routes/lumia/+page.svelte
@@ -0,0 +1,37 @@
+<h1>Privacy Policy for Lumia Torch:</h1>
+
+<p>At Lumia Torch, we are committed to protecting your privacy and ensuring the security of your personal information. This Privacy Policy outlines how we collect, use, and safeguard the data you provide when using our app. By using Lumia Torch, you agree to the terms outlined in this policy.</p>
+
+<h2>1. Information Collection and Use:</h2>
+<p>We do not collect any personally identifiable information (PII) from our users. Lumia Torch does not require any logins, registrations, or access to personal accounts. The app solely functions as a flashlight and does not gather any personal data.</p>
+
+<h2>2. Device Permissions:</h2>
+<p>Lumia Torch requires certain permissions to provide its core functionality. These permissions include camera access and flashlight control. Rest assured, we do not use these permissions to capture or transmit any data from your device. They are solely utilized to control the flashlight and provide the intended functionality of the app.
+</p>
+
+<h2>3. Non-Personal Information:</h2>
+<p>To improve our services and user experience, we may collect non-personal information in a strictly anonymized and aggregated form. This includes usage statistics, app crashes, and performance data. Such information helps us identify and resolve issues, optimize our app, and understand user preferences. None of this data is linked to any personal information.
+</p>
+
+<h2>4. Third-Party Services:</h2>
+<p>Lumia Torch does not integrate any third-party services or SDKs (Software Development Kits) that collect personal information. We prioritize user privacy and ensure that your data remains within the confines of the app.
+</p>
+
+<h2>5. Security:</h2>
+<p>We take appropriate measures to protect the security of your information. Lumia Torch utilizes industry-standard security protocols to prevent unauthorized access, disclosure, or alteration of any data under our control. However, please note that no method of transmission over the internet or electronic storage is 100% secure.
+</p>
+
+<h2>6. Children's Privacy:</h2>
+<p>Lumia Torch is not designed or intended for use by children under the age of 13. We do not knowingly collect any personal information from children. If you believe we may have inadvertently collected personal data from a child, please contact us immediately so that we can take appropriate action.
+</p>
+
+<h2>7. Changes to the Privacy Policy:</h2>
+<p>We reserve the right to update or modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the revised policy in the app. It is your responsibility to review this policy periodically for any updates.
+</p>
+
+<h2>8. Contact Us:</h2>
+<p>If you have any questions, concerns, or requests regarding this Privacy Policy or the handling of your information, please contact us at jsiziba.zw@gmail.com.
+</p>
+
+<p>By using Lumia Torch, you acknowledge that you have read and understood this Privacy Policy and agree to the collection, use, and disclosure of your information as described herein.
+</p>
