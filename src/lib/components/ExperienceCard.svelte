5d884abd8ffe615a918715a20a1046ce25fcacc3
Johnson Andrew Siziba <jsiziba.zw@gmail.com>
2023-12-15 13:41:13 +0200

    Fix allignment issue

--- src/lib/components/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard.svelte
@@ -13,7 +13,7 @@
 
 <div class="w-full flex flex-col lg:flex-row gap-3 lg:gap-8 p-5 hover:bg-highstorm-white hover:bg-opacity-5 hover:shadow rounded-sm">
   <h2 class="text-[18px] lg:text-center font-medium lg:font-light lg:text-windrunner-blue-secondary lg:w-1/3">{duration}</h2>
-  <div class="flex flex-col gap-4 flex-grow">
+  <div class="flex flex-col gap-4 w-full">
     <h2><a rel="external" target="_blank" href={companyWebsite}>{title} <span>.</span> {companyName}</a></h2>
     <p>{description}</p>
     <div class="flex gap-1 lg:gap-3 flex-wrap">
