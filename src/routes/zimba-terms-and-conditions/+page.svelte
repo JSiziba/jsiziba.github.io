5f6b83373069b788ea2b14df732ecc158394b74b
Johnson Andrew Siziba <132450388+JohnsonAtQuintessence@users.noreply.github.com>
2024-01-25 18:19:54 +0200

    Zimba terms and conditions

---
+++ src/routes/zimba-terms-and-conditions/+page.svelte
@@ -0,0 +1,152 @@
+<h1>Terms and Conditions</h1>
+
+<h2>1. Acceptance of Terms</h2>
+
+<p>By accessing or using the Zimba Properties app (the "App"), you agree to comply with and be bound by these Terms and Conditions.
+</p>
+
+<h2>2. Property Listings</h2>
+<ul>
+	<li>
+		a. Zimba Properties allows users to list properties for sale, rent, or other real estate transactions.
+
+		</li>
+	<li>b. Users are solely responsible for the accuracy and legality of the information provided in property listings.</li>
+</ul>
+
+<h2>3. User Responsibilities</h2>
+
+<ul>
+	<li>a. Users must provide accurate and up-to-date information during registration.</li>
+	<li>b. Users are responsible for maintaining the confidentiality of their account credentials.</li>
+</ul>
+
+
+<h2>4. Communication and Transactions</h2>
+
+<ul>
+	<li>a. Zimba Properties facilitates communication between property owners/agents and users. However, the app is not involved in the actual transactions.</li>
+
+	<li>b. Users are solely responsible for any agreements, transactions, or disputes arising from property listings.</li>
+</ul>
+
+
+<h2>5. Code of Conduct</h2>
+<ul>
+	<li>a. Users must not engage in any illegal or harmful activities while using the App.</li>
+	<li>b. Users must respect the privacy and rights of other users.</li>
+</ul>
+
+
+<h2>6. Intellectual Property</h2>
+
+<ul>
+	<li>a. Zimba Properties retains all rights to the App and its contents.</li>
+	<li>b. Users must not copy, modify, or distribute the App without prior written consent from Zimba Properties.</li>
+	<li>c. The content and features of the App are protected by intellectual property laws. Users may not reproduce, distribute, or modify any part of the App without explicit permission.</li>
+</ul>
+
+<h2>7. Termination</h2>
+
+<ul>
+	<li>a. Zimba Properties reserves the right to suspend or terminate user accounts at any time.</li>
+	<li>b. Users can terminate their accounts by sending a request to the email address: siziba.zw@gmail.com.
+		The account will be terminated within 90 days of receiving the request.</li>
+</ul>
+
+<h2>8. Changes to Terms</h2>
+
+<ul>
+	<li>a. Zimba Properties reserves the right to update these Terms and Conditions at any time.</li>
+	<li>b. Users will be notified of significant changes to the Terms and Conditions.</li>
+</ul>
+
+<h2>9. Contact Information</h2>
+<p>
+	For any inquiries or concerns regarding the Terms and Conditions, users can contact Zimba Properties at jsiziba.zw@gmail.com
+</p>
+
+<h1>Privacy Policy</h1>
+
+<h2>1. Information Collection</h2>
+<ul>
+	<li>a. Zimba Properties collects user information, including but not limited to, name, contact details, and property details.</li>
+	<li>b. Information may be collected through user registration, property listings, and communication on the app.</li>
+</ul>
+
+
+
+<h2>2. Use of Information</h2>
+<ul>
+	<li>a. Zimba Properties uses user information to provide services and improve the App.</li>
+	<li>b. Zimba Properties may use aggregated, anonymized data for analytical purposes.</li>
+</ul>
+
+
+<h2>3. Data Security</h2>
+<ul>
+	<li>a. Zimba Properties employs reasonable security measures to protect user information.</li>
+	<li>b. Users are responsible for maintaining the security of their account credentials.</li>
+</ul>
+
+<h2>4. Third-Party Services</h2>
+
+<ul>
+	<li>a. Zimba Properties may use third-party services for analytics, advertising, and other purposes. Users are subject to the privacy policies of these services.</li>
+</ul>
+
+<h2>5. Legal Compliance</h2>
+
+<ul>
+	<li>a. Zimba Properties complies with applicable data protection laws.</li>
+	<li>b. User information may be disclosed in response to legal requests or to protect the rights and safety of Zimba Properties, its users, and others.</li>
+</ul>
+
+<h2>6. Contact Information</h2>
+<ul>
+	<li>
+		a. For any inquiries or concerns regarding privacy, users can contact Zimba Properties at jsiziba.zw@gmail.com
+	</li>
+</ul>
+
+
+<h2>
+	7. Changes to Privacy Policy
+</h2>
+
+<p>
+
+Zimba Properties reserves the right to update this Privacy Policy. Users will be notified of significant changes.
+
+</p>
+
+
+<style>
+	h1	{
+			margin-bottom: 30px;
+			margin-top: 20px;
+			font-size: 32px;
+			font-weight: bold;
+	}
+
+	h2	{
+			margin-bottom: 20px;
+			margin-top: 20px;
+			font-weight: normal;
+			font-size: 32px;
+	}
+
+	ul {
+			list-style-type: disc;
+			margin-left: 35px;
+	}
+
+
+  li {
+		margin-top: 5px;
+		margin-bottom: 5px;
+  }
+
+
+
+</style>
