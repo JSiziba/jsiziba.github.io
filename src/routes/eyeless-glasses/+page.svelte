6d98ff38ec95dc9c71fe5848184c14332f1b3d88
Johnson Andrew Siziba <132450388+JohnsonAtQuintessence@users.noreply.github.com>
2024-02-08 16:43:54 +0200

    Eyeless glasses

---
+++ src/routes/eyeless-glasses/+page.svelte
@@ -0,0 +1,124 @@
+<svelte:head>
+	<title>Eyeless Glasses Privacy Policy and Terms of Use</title>
+</svelte:head>
+
+
+<p class="text-[55px] text-center mb-[50px]">
+	Eyeless Glasses Privacy Policy and Terms of Use
+</p>
+
+<h1>Privacy Policy</h1>
+
+<h2>Information We Collect:</h2>
+
+<p>Camera and Media Access: When you use our object detection and OCR features, we may access your device's camera and media files to provide the intended functionalities.</p>
+
+<h2>How We Use Your Information:</h2>
+
+<p>Camera and media access enable us to deliver object detection and OCR functionalities accurately.</p>
+
+<h2>Data Security and Retention:</h2>
+
+<p>We do not retain any data from your camera or media files on our servers. As soon as the object detection or OCR functionalities are performed, the data is discarded.</p>
+
+<h2>Third-Party Services:</h2>
+
+<p>We use no third-party services to collect or process your data.</p>
+
+<h2>Changes to This Privacy Policy:</h2>
+
+<p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.</p>
+
+<h2>Contact Us:</h2>
+
+<p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at <a href="mailto:jsiziba.zw@gmail.com">
+	jsiziba.zw@gmail.com
+</a>
+</p>
+
+<h1>Terms of Use</h1>
+
+<h2>1. Introduction</h2>
+
+<p>These Terms of Use govern your use of this mobile application. By using this application, you accept these Terms of Use in full. If you disagree with these Terms of Use or any part of these Terms of Use, you must not use this application.</p>
+
+<h2>2. Intellectual Property Rights</h2>
+
+<p>Other than the content you own, under these Terms, Eyeless Glasses and/or its licensors own all the intellectual property rights and materials contained in this application.</p>
+
+<p>You are granted a limited license only for purposes of viewing the material contained on this application.</p>
+
+<h2>3. Restrictions</h2>
+
+<p>You are specifically restricted from all of the following:</p>
+
+<ul>
+	<li>publishing any application material in any other media;</li>
+	<li>selling, sublicensing and/or otherwise commercializing any application material;</li>
+	<li>publicly performing and/or showing any application material;</li>
+	<li>using this application in any way that is or may be damaging to this application;</li>
+	<li>using this application in any way that impacts user access to this application;</li>
+	<li>using this application contrary to applicable laws and regulations, or in any way may cause harm to the application, or to any person or business entity;</li>
+	<li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this application;</li>
+	<li>using this application to engage in any advertising or marketing.</li>
+</ul>
+
+
+<h2>4. No Warranties</h2>
+
+<p>This application is provided "as is," with all faults, and Eyeless Glasses makes no express or implied representations or warranties, of any kind related to this application or the materials contained on this application. Additionally, nothing contained on this application shall be interpreted as advising you.</p>
+
+<h2>5. Limitation of Liability</h2>
+
+<p>In no event shall Eyeless Glasses, nor any of its officers, directors, developers, and employees be held liable for anything arising out of or in any way connected with your use of this application whether such liability is under contract. Eyeless Glasses, including its officers, directors, developers, and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this application.</p>
+
+<h2>6. Indemnification</h2>
+
+<p>You hereby indemnify to the fullest extent Eyeless Glasses from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
+
+<h2>7. Severability</h2>
+
+<p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
+
+<h2>8. Variation of Terms</h2>
+
+<p>Eyeless Glasses is permitted to revise these Terms at any time as it sees fit, and by using this application you are expected to review these Terms on a regular basis.</p>
+
+<h2>9. Assignment</h2>
+
+<p>The Eyeless Glasses is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+
+<h2>10. Entire Agreement</h2>
+
+<p>These Terms constitute the entire agreement between Eyeless Glasses and you in relation to your use of this application, and supersede all prior agreements and understandings.</p>
+
+
+<style>
+	h1	{
+			margin-bottom: 30px;
+			margin-top: 20px;
+			font-size: 32px;
+			font-weight: bold;
+	}
+
+	h2	{
+			margin-bottom: 20px;
+			margin-top: 20px;
+			font-weight: normal;
+			font-size: 32px;
+	}
+
+	ul {
+			list-style-type: disc;
+			margin-left: 35px;
+	}
+
+
+  li {
+		margin-top: 5px;
+		margin-bottom: 5px;
+  }
+
+
+
+</style>
