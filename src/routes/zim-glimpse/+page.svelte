c201ddaaac8945f56c1e9f7b5cdd7cc30f31c94f
Johnson Andrew Siziba <132450388+JohnsonAtQuintessence@users.noreply.github.com>
2024-02-20 11:08:00 +0200

    Zim glimpse

---
+++ src/routes/zim-glimpse/+page.svelte
@@ -0,0 +1,147 @@
+<svelte:head>
+	<title>Zim Glimpse Terms of Use and Privacy Policy</title>
+</svelte:head>
+
+
+<p class="text-[55px] text-center mb-[50px]">
+	Zim Glimpse Terms of Use and Privacy Policy
+</p>
+
+<h1>Terms of Use</h1>
+
+<h2>Introduction</h2>
+
+<p>
+	Welcome to Zim Glimpse! These Terms of Use govern your use of our application and services. By accessing or using Zim Glimpse, you agree to be bound by these terms. If you do not agree to these terms, please do not use the app.
+</p>
+
+<h2>Service Description</h2>
+<p>
+	Zim Glimpse is a news aggregation application that curates headlines from various Zimbabwean publications and international sources, focusing on news related to Zimbabwe.
+</p>
+
+<h2>User Responsibilities</h2>
+<ul>
+	<li>
+		Users must use Zim Glimpse for lawful purposes only and comply with all applicable laws and regulations.
+	</li>
+	<li>
+		Redistribution of content obtained from Zim Glimpse is not permitted without prior written consent from the original content creators.
+	</li>
+	<li>
+		Users must not use the app in any way that could impair its functionality or availability.
+	</li>
+</ul>
+
+<h2>Intellectual Property</h2>
+<p>
+	Content displayed on Zim Glimpse remains the intellectual property of the original publishers. The app serves as a medium to aggregate and present this content in a summarized format.
+</p>
+<p>
+	Users are not permitted to reproduce, modify, or distribute content obtained from Zim Glimpse without prior written consent from the original content creators.
+</p>
+
+<h2>Disclaimer</h2>
+<p>
+	Zim Glimpse is provided "as is" and "as available" without any warranties of any kind. We do not guarantee the accuracy, completeness, or timeliness of the information presented in the app.
+</p>
+<p>
+	We are not responsible for any loss or damage resulting from the use of Zim Glimpse.
+</p>
+<p>
+	The information provided through Zim Glimpse is for general informational purposes only. We do not warrant the accuracy, completeness, or usefulness of this information.
+</p>
+
+<h2>Limitation of Liability</h2>
+<p>
+	Zim Glimpse and its creators will not be liable for any damages arising out of the use or inability to use the application.
+</p>
+<p>
+	We are not responsible for any loss or damage resulting from the use of Zim Glimpse.
+</p>
+
+<h2>Changes to Terms</h2>
+<p>
+	We reserve the right to modify these terms at any time. If we make changes, we will post the updated terms on this page.
+</p>
+<p>
+	Your continued use of Zim Glimpse after any such changes constitutes your acceptance of the new terms.
+</p>
+
+<h1>
+	Privacy Policy
+</h1>
+
+<h2>Introduction</h2>
+<p>
+	We respect your privacy and are committed to protecting it through our compliance with this policy. This policy describes the types of information we may collect from you or that you may provide when you use Zim Glimpse and our practices for collecting, using, maintaining, protecting, and disclosing that information.
+</p>
+
+<h2>Information We Collect</h2>
+<ul>
+	<li>
+		Personal Information: We do not collect personal information unless you provide it to us voluntarily (e.g., by contacting us).
+	</li>
+	<li>
+		Usage Data: We may collect data on how the app is accessed and used to improve service quality.
+	</li>
+</ul>
+
+<h2>How We Use Your Information</h2>
+<p>
+	The information we collect is used to provide and improve the service, troubleshoot, and perform analytics.
+</p>
+
+<h2>Data Sharing</h2>
+<p>
+	We do not share personal information with third parties except as required by law or to protect our rights.
+</p>
+
+<h2>Data Security</h2>
+<p>
+	We strive to use commercially acceptable means to protect your information but cannot guarantee its absolute security.
+</p>
+
+<h2>
+	Changes to Privacy Policy
+</h2>
+<p>
+	We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+</p>
+
+<h2>
+	Contact Us
+</h2>
+<p>
+	If you have any questions about these Terms of Use or Privacy Policy, please contact us at <a href="mailto:jsiziba.zw@gmail.com">jsiziba.zw@gmail.com</a>
+</p>
+
+<style>
+	h1	{
+			margin-bottom: 30px;
+			margin-top: 20px;
+			font-size: 32px;
+			font-weight: bold;
+	}
+
+	h2	{
+			margin-bottom: 20px;
+			margin-top: 20px;
+			font-weight: normal;
+			font-size: 32px;
+	}
+
+	ul {
+			list-style-type: disc;
+			margin-left: 35px;
+	}
+
+
+  li {
+		margin-top: 5px;
+		margin-bottom: 5px;
+  }
+
+
+
+</style>
